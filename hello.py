4e8d15b4e79c166a98a966c504f65eee0f4c842e
Bill Baker <bill@baker88.com>
2020-04-22 11:40:06 -0700

    Completed first program and tested

---
+++ hello.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+Bill Baker
+First Program lab
+Hello World Program
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Hello World!")
+    print()
+
+main()    
